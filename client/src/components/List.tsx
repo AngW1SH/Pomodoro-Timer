445deeb78cce440b86d35c17afe87e83ff77216d
Denis Laluev <113942822+AngW1SH@users.noreply.github.com>
2023-04-07 22:46:37 +0300

    add list desktop visuals

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -3,7 +3,82 @@ import React, { FC } from "react";
 interface ListProps {}
 
 const List: FC<ListProps> = () => {
-  return <div className="h-full w-7/12">456</div>;
+  return (
+    <div className="h-full w-7/12 overflow-auto p-10">
+      <div className="relative mb-6 flex cursor-pointer items-end justify-center border border-black py-4">
+        <div className="ml-4 mr-5 whitespace-nowrap text-xl font-bold">
+          Title
+        </div>
+        <div className="mr-20 overflow-hidden whitespace-nowrap">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          nemo eaque ratione ut? Ab dignissimos ut, incidunt omnis hic sapiente.
+        </div>
+        <div className="absolute right-0 top-0 flex h-full w-14 items-center justify-center border-l border-black bg-white">
+          1
+        </div>
+      </div>
+      <div className="relative mb-6 flex cursor-pointer items-end border border-black py-4">
+        <div className="ml-4 mr-5 whitespace-nowrap text-xl font-bold">
+          Title
+        </div>
+        <div className="mr-20 overflow-hidden whitespace-nowrap">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          nemo eaque ratione ut? Ab dignissimos ut, incidunt omnis hic sapiente.
+        </div>
+        <div className="absolute right-0 top-0 flex h-full w-14 items-center justify-center border-l border-black bg-white">
+          1
+        </div>
+      </div>
+      <div className="relative mb-6 flex cursor-pointer items-end border border-black py-4">
+        <div className="ml-4 mr-5 whitespace-nowrap text-xl font-bold">
+          Title
+        </div>
+        <div className="mr-20 overflow-hidden whitespace-nowrap">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          nemo eaque ratione ut? Ab dignissimos ut, incidunt omnis hic sapiente.
+        </div>
+        <div className="absolute right-0 top-0 flex h-full w-14 items-center justify-center border-l border-black bg-white">
+          1
+        </div>
+      </div>
+      <div className="relative mb-6 flex cursor-pointer items-end border border-black py-4">
+        <div className="ml-4 mr-5 whitespace-nowrap text-xl font-bold">
+          Title
+        </div>
+        <div className="mr-20 overflow-hidden whitespace-nowrap">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          nemo eaque ratione ut? Ab dignissimos ut, incidunt omnis hic sapiente.
+        </div>
+        <div className="absolute right-0 top-0 flex h-full w-14 items-center justify-center border-l border-black bg-white">
+          1
+        </div>
+      </div>
+      <div className="relative mb-6 flex cursor-pointer items-end border border-black py-4">
+        <div className="ml-4 mr-5 whitespace-nowrap text-xl font-bold">
+          Title
+        </div>
+        <div className="mr-20 overflow-hidden whitespace-nowrap">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          nemo eaque ratione ut? Ab dignissimos ut, incidunt omnis hic sapiente.
+        </div>
+        <div className="absolute right-0 top-0 flex h-full w-14 items-center justify-center border-l border-black bg-white">
+          1
+        </div>
+      </div>
+      <div className="relative mb-6 flex cursor-pointer items-end border border-black py-4">
+        <div className="ml-4 mr-5 whitespace-nowrap text-xl font-bold">
+          Title
+        </div>
+        <div className="mr-20 overflow-hidden whitespace-nowrap">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          nemo eaque ratione ut? Ab dignissimos ut, incidunt omnis hic sapiente.
+        </div>
+        <div className="absolute right-0 top-0 flex h-full w-14 items-center justify-center border-l border-black bg-white">
+          1
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
