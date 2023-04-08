c33c14d660878d9d676524543f3dea78b9b4dcf7
Denis Laluev <113942822+AngW1SH@users.noreply.github.com>
2023-04-08 16:59:34 +0300

    add error handling to the backend

--- server/router/api-router.ts
+++ server/router/api-router.ts
@@ -7,34 +7,59 @@ const prisma = new PrismaClient();
 //const prisma = new PrismaClient();
 
 apiRouter.post("/add", async (req, res) => {
-  const add = await prisma.pomodoro.create({
-    data: req.body.pomodoro,
-  });
-  res.status(200).send(add);
+  if (!req.body.pomodoro) {
+    res.status(400).send("Pomodoro not recieved");
+  }
+  try {
+    const add = await prisma.pomodoro.create({
+      data: req.body.pomodoro,
+    });
+    res.status(200).send(add);
+  } catch (error) {
+    res.status(500);
+  }
 });
 
 apiRouter.post("/save", async (req, res) => {
-  const add = await prisma.pomodoro.update({
-    where: {
-      id: req.body.pomodoro.id,
-    },
-    data: req.body.pomodoro,
-  });
-  res.status(200).send(add);
+  if (!req.body.pomodoro) {
+    res.status(400).send("Pomodoro not recieved");
+  }
+  try {
+    const add = await prisma.pomodoro.update({
+      where: {
+        id: req.body.pomodoro.id,
+      },
+      data: req.body.pomodoro,
+    });
+    res.status(200).send(add);
+  } catch (error) {
+    res.status(500);
+  }
 });
 
 apiRouter.post("/delete", async (req, res) => {
-  const deleted = await prisma.pomodoro.delete({
-    where: {
-      id: req.body.id,
-    },
-  });
-  res.status(200).send(deleted);
+  if (!req.body.id) {
+    res.status(400).send("Id not recieved");
+  }
+  try {
+    const deleted = await prisma.pomodoro.delete({
+      where: {
+        id: req.body.id,
+      },
+    });
+    res.status(200).send(deleted);
+  } catch (error) {
+    res.status(500);
+  }
 });
 
 apiRouter.get("/get", async (req, res) => {
-  const pomodoros = await prisma.pomodoro.findMany();
-  res.status(200).send(pomodoros);
+  try {
+    const pomodoros = await prisma.pomodoro.findMany();
+    res.status(200).send(pomodoros);
+  } catch (error) {
+    res.status(500);
+  }
 });
 
 export default apiRouter;
